8d70a7c19baa89565688054ecc880ef36e607c07
{Andrew Deacy} <{andrew.deacy@partner.bmwgroup.com}>
2018-07-25 10:58:41 -0500

    Added custom header, using bootstrap now too

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
+import { Navbar, MenuItem, NavDropdown, Nav, NavItem } from 'react-bootstrap';
 import * as React from 'react';
-import './App.css';
+
 
 import headerLogo from './header-logo.png';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="">
+        <div>
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#home">
+                        <img src={headerLogo} className="app-header-logo" alt="logo" height={25}/>
+                    </a>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+                <NavItem eventKey={1} href="#">
+                    Link
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                    Link
+                </NavItem>
+                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                    <MenuItem eventKey={3.1}>Action</MenuItem>
+                    <MenuItem eventKey={3.2}>Another action</MenuItem>
+                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                    <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                </NavDropdown>
+            </Nav>
+        </Navbar>
 
-        <header className="app-header">
-          <img src={headerLogo} className="app-logo" alt="logo" />
-        </header>
 
 
       </div>
