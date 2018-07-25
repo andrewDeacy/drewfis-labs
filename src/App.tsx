0775afc172cca954f7d3be6d923786fb82fccbfd
{Andrew Deacy} <{andrew.deacy@partner.bmwgroup.com}>
2018-07-25 09:45:52 -0500

    Added google's material UI + drewfis labs logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+import headerLogo from './header-logo.png';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="">
+
+        <header className="app-header">
+          <img src={headerLogo} className="app-logo" alt="logo" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
+
       </div>
     );
   }
